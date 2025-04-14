0456e7f601e97f9facea734e1ccad6d3cd6b8553
Thien Huynh <thien.huynh@saigontechnology.com>
2025-04-14 12:00:41 +0700

    chore: remove rbenv

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -67,9 +67,6 @@ if not string match -q -- $PNPM_HOME $PATH
 end
 # pnpm end
 
-eval "$(/opt/homebrew/bin/brew shellenv)"
-status --is-interactive; and rbenv init - fish | source
-
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH

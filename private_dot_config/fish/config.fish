102e7ccbb701fe2fcdf639439a9683eccd30dba6
Thien Huynh <thuynh@radicle-health.com>
2025-04-14 13:24:39 +0700

    chore: minor update

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -45,7 +45,8 @@ alias gs "git status"
 alias gc "git checkout"
 alias gd "git diff"
 alias gfa "git fetch --all"
-alias grh "git reset --hard"
+alias grd "git reset --hard origin/dev"
+alias grm "git reset --hard origin/main"
 alias gcm "git commit -m"
 
 alias tf terraform

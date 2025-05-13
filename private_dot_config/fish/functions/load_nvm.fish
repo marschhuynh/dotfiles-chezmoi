22885783e6632c26d3f984aaa10c51441dee387d
Thien Huynh <thien.huynh@saigontechnology.com>
2025-05-13 17:07:33 +0700

    fix: load_nvm function

--- private_dot_config/fish/functions/load_nvm.fish
+++ private_dot_config/fish/functions/load_nvm.fish
@@ -2,16 +2,15 @@ function load_nvm --on-variable="PWD"
   set -l default_node_version (nvm current default)
   set -l node_version (nvm current)
   set -l nvmrc_path (nvm_find_nvmrc)
-  echo $nvmrc_path
   if test -n "$nvmrc_path"
-    set -l nvmrc_node_version (cat $nvmrc_path)
-    if test "$nvmrc_node_version" = "N/A"
+    set -l nvmrc_node_version (nvm list | grep (cat $nvmrc_path))
+    if test "$nvmrc_node_version" = ""
       nvm install (cat $nvmrc_path)
     else if test "$nvmrc_node_version" != "$node_version"
-      nvm use $nvmrc_node_version
+      nvm use
     end
   else if test "$node_version" != "$default_node_version"
     echo "Reverting to default Node version"
-    nvm use default
+    nvm use
   end
 end
